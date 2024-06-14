01f15e51a74299f6fb266cd9e6da2d4003baa889
buru99 <155699998+buru99@users.noreply.github.com>
2024-06-14 12:49:31 -0300

    Update my_site.py

--- my_site.py
+++ my_site.py
@@ -1,11 +1,28 @@
 import flet as ft
-
-
+import requests
+import datetime
 
 def main(page: ft.Page):
-    page.title="meu app flet"
-    def link(e):
-        page.launch_url("https://www.youtube.com/")
-    botao = ft.TextButton("link", on_click=link)
-    page.add(ft.Text("Meu aplicativo"), botao)
+    page.title = "Controle de validade digital"
+    produtos = []
+    produtos_list_view = ft.ListView()
+    #partes visuais
+    desc = ft.TextField(label="Descrição")
+    ean = ft.TextField(label="EAN do Produto")
+    quantidade = ft.TextField(label="Quantidade")
+    data = ft.DatePicker(datetime.datetime.now())
+    #o botão salvar fica dentro do pageButton
+    salvar = ft.TextButton(icon=ft.icons.SAVE, text="Salvar", on_click="")
+    page.bottom_appbar = ft.BottomAppBar(bgcolor=ft.colors.TRANSPARENT, content=ft.Row(controls=[salvar]))
+    add_button = ft.FloatingActionButton("Adicionar", on_click="" ,width=100)
+    cleanAllButton = ft.FloatingActionButton("Limpar Lista", on_click="", width=100)
+    date_button = ft.ElevatedButton("Validade do Produto", icon=ft.icons.CALENDAR_MONTH, on_click=lambda _: data.pick_date())
+    list_container = ft.Container(content=produtos_list_view, height=300, expand=True)
+
+    #adiciona os itens a tela
+    page.overlay.append(data)
+    page.add(ft.Column([desc, ean, quantidade, date_button]))
+    page.add(ft.Row([add_button, cleanAllButton]))
+    page.add(list_container)
 ft.app(main)
+    
